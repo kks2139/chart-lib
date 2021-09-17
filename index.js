25b88d900016b24059b70f7ad90a9088221448d3
kwangsun <eyeyel@naver.com>
2021-09-18 00:32:26 +0900

    add point

--- index.js
+++ index.js
@@ -18,46 +18,67 @@ const PADDING_Y = 80;
 class MyChart{
     constructor({canvas, yLabel, xLabel, datas}){
         this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
         this.clientWidth = canvas.clientWidth;
         this.clientHeight = canvas.clientHeight;
         this.yLabel = yLabel;
         this.xLabel = xLabel;
         this.datas = datas;
-        this.ctx = canvas.getContext('2d');
+        // 계산된 x, y축 라벨 좌표
+        this.yLabelPos = {};
+        this.xLabelPos = [];
         
         this.drwaChart();
     }
 
     setAxis = ()=>{
+        const {ctx} = this;
+
         // x, y축 그리기
         const xLength = this.clientWidth - PADDING_X;
         const yLength = this.clientHeight - PADDING_Y;
-        this.ctx.moveTo(PADDING_X, PADDING_Y);
-        this.ctx.lineTo(PADDING_X, yLength);
-        this.ctx.moveTo(PADDING_X, yLength);
-        this.ctx.lineTo(xLength, yLength);
-        this.ctx.stroke();
+        ctx.moveTo(PADDING_X, PADDING_Y);
+        ctx.lineTo(PADDING_X, yLength);
+        ctx.moveTo(PADDING_X, yLength);
+        ctx.lineTo(xLength, yLength);
+        ctx.stroke();
 
-        // x, y축 라벨링
-        this.ctx.font = '20px bold';
-        this.ctx.textAlign = 'right';
+        this.yLabelPos.baseLine = PADDING_Y + yLength;
+
+        // y축 라벨링
+        ctx.font = '20px bold';
+        ctx.textAlign = 'right';
         this.yLabel.forEach((d, i, arr) =>{
             const interval = Math.floor(yLength / arr.length - 3) * i;
-            this.ctx.fillText(d, PADDING_X - 10, yLength - interval + 10);
+            const yPos = yLength - interval;
+            ctx.fillText(d, PADDING_X - 10, yPos);
         });
-        this.ctx.textAlign = 'center';
+
+        // x축 라벨링
+        ctx.textAlign = 'center';
         this.xLabel.forEach((d, i, arr) =>{
             const startX = 50;
             const interval = Math.floor(xLength / arr.length - Math.floor(startX / 6)) * i;
-            this.ctx.fillText(d, startX + PADDING_X + interval, yLength + 25);
+            const xPos = startX + PADDING_X + interval;
+            ctx.fillText(d, xPos, yLength + 25);
+            this.xLabelPos.push(xPos);
         });
     }
 
     setData = ()=>{
+        const {ctx, datas, xLabelPos, yLabelPos} = this;
 
+        ctx.fillStyle = 'yellow';
+        datas.forEach((d, i) =>{
+            ctx.beginPath();
+            ctx.arc(xLabelPos[i], 300, 5, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
+        });
     }
 
     drwaChart = ()=>{
         this.setAxis();
+        this.setData();
     }
 }
