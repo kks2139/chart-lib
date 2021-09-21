a10256bf2890359022950a494a482e8cb2b30cf9
kwangsun <eyeyel@naver.com>
2021-09-21 21:56:20 +0900

    add line chart

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ window.onload = ()=>{
     const yLabel = [0,10,20,30,40,50,60,70,80,90,100];
     const xLabel = ['국어','영어','수학','과학','미술','체육'];
     const datas = [55,60,90,95,80,88];
+    const rectWidth = 30;
 
     new MyChart({
+        chart: 'line',
         canvas,
         yLabel,
         xLabel,
-        datas
+        datas,
+        rectWidth
     });
 }
 
@@ -16,7 +19,8 @@ const PADDING_X = 80;
 const PADDING_Y = 80;
 
 class MyChart{
-    constructor({canvas, yLabel, xLabel, datas}){
+    constructor({chart, canvas, yLabel, xLabel, datas, rectWidth=20}){
+        this.chart = chart;
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.clientWidth = canvas.clientWidth;
@@ -24,6 +28,7 @@ class MyChart{
         this.yLabel = yLabel;
         this.xLabel = xLabel;
         this.datas = datas;
+        this.rectWidth = rectWidth;
         // 계산된 x, y축 라벨 좌표
         this.yLabelPos = {
             baseLine: 0,
@@ -69,19 +74,65 @@ class MyChart{
         });
     }
 
-    setData = ()=>{
+    
+    setColumnChart = ()=>{
         const {ctx, datas, xLabelPos, yLabelPos} = this;
         const {baseLine, interval} = yLabelPos;
-
+        
         ctx.fillStyle = 'yellow';
         datas.forEach((d, i) =>{
             const value = baseLine - (d * interval / 10);
-
+    
             ctx.beginPath();
-            ctx.arc(xLabelPos[i], value, 5, 0, 2 * Math.PI);
+            ctx.rect(xLabelPos[i] - (this.rectWidth / 2), value, this.rectWidth, baseLine - value);
             ctx.fill();
             ctx.stroke();
         });
+    }
+
+    setLineChart = ()=>{
+        const {ctx, datas, xLabelPos, yLabelPos} = this;
+        const {baseLine, interval} = yLabelPos;
+        
+        ctx.fillStyle = 'yellow';
+        datas.forEach((d, i, arr) =>{
+            const preValue = baseLine - (arr[i-1] * interval / 10);
+            const value = baseLine - (d * interval / 10);
+    
+            ctx.beginPath();
+            ctx.arc(xLabelPos[i], value, 8, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
+
+            if(i > 0){
+                ctx.beginPath();
+                ctx.moveTo(xLabelPos[i-1], preValue);
+                ctx.lineTo(xLabelPos[i], value);
+                ctx.stroke();
+            }
+        });
+    }
+
+    setPieChart = ()=>{
+
+    }
+
+    setData = ()=>{
+        const {chart} = this;
+        switch(chart){
+            case 'column': 
+                this.setColumnChart();
+                break;
+            case 'line': 
+                this.setLineChart();
+                break;
+            case 'pie': 
+                this.setPieChart();
+                break;
+            default:
+                console.error(`'${chart}' is not defined chart`);
+                break;
+        }
     }
 
     drwaChart = ()=>{
